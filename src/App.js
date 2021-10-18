d6e9e17001e960e06cc7501f7443873a24c0ab68
FcoJavier0099 <36982961+FcoJavier0099@users.noreply.github.com>
2021-10-17 21:48:29 -0500

    Inicio enrutamiento unificado

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './App.css';
 import {Header} from './components/Header'
 import {Menu} from './components/Menu'
-import {Content} from './components/Content'
+import { Content } from './components/Content'
 
-document.addEventListener('DOMContentLoaded', ()  =>{
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Productos } from './components/sections/Productos';
+import { Usuarios } from './components/sections/Usuarios';
+import { Dashboard } from './components/sections/Dashboard';
+import { login } from './components/sections/login';
+import { Ventas } from './components/sections/Ventas';
+
+  document.addEventListener('DOMContentLoaded', ()  =>{
   const btnMenu = document.getElementById('btn-drop-menu')
   const menu = document.getElementById('menu')
   const content = document.getElementById('content')
@@ -27,12 +35,33 @@ document.addEventListener('DOMContentLoaded', ()  =>{
 
 function App() {
   return (
-
     <div className="container">
       <Header />
       <div className="row px-2">
         <Menu />
-        <Content />
+        {/* <Content /> */}
+        <div id="content" className="col-9">
+          <div className="row justify-content-end">
+            <div className="col-11 content d-flex flex-column justify-content-center">
+              <Router>
+                <Switch>
+                  <Route path="/productos">
+                    <Productos />
+                  </Route>
+                  <Route path="/usuarios">
+                    <Usuarios />
+                  </Route>
+                  <Route path="/ventas">
+                    <Ventas />
+                  </Route>
+                  <Route path="/">
+                    <login />
+                  </Route>
+                </Switch>
+              </Router>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,41 +1,53 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  RouterContext,
+  useHistory
+} from "react-router-dom";
 
+export const Menu = ()=>{
+  let history = useHistory()
+  const irUsuarios = (e) => {
+    e.preventDefault()
+    history.push("/usuarios")
+  }
 
-export const Menu = () => {
   return (
     <div id="menu" className="menu col-3 menu-animations-outside">
       <button id="btn-drop-menu" className="drop-menu">
         <img src="../img/up-arrow-angle.png" alt="" width="50" />
       </button>
+      <Router>
       <div className="row text-start justify-content-center">
-        <div className="col-12">
+        <div className="col-12" >
           <h2>Ventas</h2>
           <ul>
-            <a href="ventasRealizadas.html">
-              <li>Ventas Realizadas</li>
-            </a>
-            <a href="registrar_venta.html">
-              <li>Registrar Ventas</li>
-            </a>
+            <li><Link to="/ventas">Ventas Realizadas JAAA</Link></li>
+              <li><Link to={"/usuarios"}>Usuarios</Link></li>
+              <a onClick={irUsuarios} href="#"><li>Ventas Realizadas B</li></a>
+            <a href="ventasRealizadas.html"><li>Ventas Re gti alizadas</li></a>
+            <a href="ventasRealizadas.html"><li>Ventas Re gti alizadas</li></a>
+            <a href="registrar_venta.html"><li>Registrar Ventas</li></a>
           </ul>
         </div>
-        <div className="col-12">
+        <div className="col-12" >
           <h2>Productos</h2>
           <ul>
-            <a href="">
-              <li>Productos</li>
-            </a>
+            <a href="listarproductos.html"><li>Productos</li></a>
+            <a href="productoRegistrar.html"><li>Registrar Productos</li></a>
           </ul>
         </div>
-        <div className="col-12">
+        <div className="col-12" >
           <h2>Usuarios</h2>
           <ul>
-            <a href="usuarios.html">
-              <li>Lista de Usuarios</li>
-            </a>
+            <a href="usuarios.html"><li>Lista de Usuarios</li></a>
           </ul>
         </div>
       </div>
+      </Router>      
     </div>
-  );
-};
+  )
+}
