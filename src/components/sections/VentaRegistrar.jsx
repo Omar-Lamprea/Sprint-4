1b07af3bc24108acaa87c0f253d86af86dfe27bd
unknown <you@example.com>
2021-10-19 21:02:37 -0500

    ventasRealizadas

--- src/components/sections/VentaRegistrar.jsx
+++ src/components/sections/VentaRegistrar.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export const VentaRegistrar = () => {
     return (
         <>
-         <h2>Registrar Venta</h2>   
+         <h2>Registrar Venta</h2>
+
         </>
     )
 }
